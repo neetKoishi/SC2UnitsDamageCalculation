129a170af99ed9aa45801656ba0dd9e0252fba17
Koishi <Koishi@Koishi.com>
2022-10-06 12:54:30 +0800

    完成sc_dict函数的制作

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import pandas as pd
 from ast import literal_eval
 from Uproperty import *
 
-
 csv = pd.read_csv('scPdata.csv', )
 # print(csv)
 csv_dict = csv.to_dict(orient='list')
@@ -14,12 +13,11 @@ for i in range(len(name_list)):
     sc_dict_temp = {}
     for k in csv_dict:
         sc_dict_temp[k] = csv_dict[k][i]
-        if k in ('atk','upgrade_atk','type_label'):
+        if k in ('atk', 'upgrade_atk', 'type_label'):
             sc_dict_temp[k] = literal_eval(csv_dict[k][i])
     temp_arr.append(sc_dict_temp)
 for i in temp_arr:
     print(i)
-
 
 # 用literal_eval 转里面的字符串为字典
 # 按间距中的绿色按钮以运行脚本。
